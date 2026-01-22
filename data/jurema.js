ba9658b23514e4c59e028a6dff6666a2840bcc92
Kethuly Aline <66758415+thekitket@users.noreply.github.com>
2026-01-22 02:58:16 -0300

    Update jurema.js

--- data/jurema.js
+++ data/jurema.js
@@ -6,22 +6,18 @@ const acervoJurema = [
         nome: "Mestre Zé Pelintra",
         pontos: [
           {titulo:"Chapéu Amarelo...", audio:"audios/jurema/mestre/01-mestre-ze-pelintra", letra:"Chapéu amarelo que vem do Pará..."},
-          {titulo:"Outro Ponto", audio:"assets/audio/jurema/mestres/outro", letra:"Letra do outro ponto..."}
-          {titulo:"Outro Ponto", audio:"assets/audio/jurema/mestres/outro", letra:"Letra do outro ponto..."}
-        ] 
-      }
-     
-    ]
-    
-  },
-  {
+          {titulo:"Outro Ponto", audio:"assets/audio/jurema/mestres/outro", letra:"Letra do outro ponto..."},
+          {titulo:"Outro Ponto 2", audio:"assets/audio/jurema/mestres/outro2", letra:"Letra do outro ponto 2..."}
+        ]
+      },
+      {
         nome: "Mestre Quebra Pedra",
         pontos: [
-          {titulo:"Do Pina...", audio:"audios/jurema/mestre/01-mestre-ze-pelintra", letra:"Chapéu amarelo que vem do Pará..."},
-          {titulo:"Outro Ponto", audio:"assets/audio/jurema/mestres/outro", letra:"Letra do outro ponto..."}
-          {titulo:"Outro Ponto", audio:"assets/audio/jurema/mestres/outro", letra:"Letra do outro ponto..."}
-               ] 
-      }     
+          {titulo:"Do Pina...", audio:"audios/jurema/mestre/01-mestre-quebra-pedra", letra:"Do Pina a Boa Viagem"},
+          {titulo:"Outro Ponto", audio:"assets/audio/jurema/mestres/outro", letra:"Letra do outro ponto..."},
+          {titulo:"Outro Ponto 2", audio:"assets/audio/jurema/mestres/outro2", letra:"Letra do outro ponto 2..."}
+        ]
+      }
     ]
   },
   {
