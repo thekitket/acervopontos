8ff7c83c973fe545fcb9a2393afa2d2da2f02e27
Kethuly Aline <66758415+thekitket@users.noreply.github.com>
2026-01-22 07:33:28 -0300

    Update jurema.js

--- data/jurema.js
+++ data/jurema.js
@@ -1,1 +1,105 @@
-const acervoJurema = [ { nome: "Mestres", entidades: [ { nome: "Mestre Cagaruçu", pontos: [ {titulo:"Ponto 01", audio:"audios/jurema/mestre/XXXXXXXXX", letra:"Letra do ponto..."}, {titulo:"Ponto 02", audio:"audios/jurema/mestre/XXXXXXXXX", letra:"Letra do ponto..."}, ] }, { nome: "Mestre Zé de Santana", pontos: [ {titulo:"Ponto 01", audio:"audios/jurema/mestre/XXXXXXXXX", letra:"Letra do ponto..."}, {titulo:"Ponto 02", audio:"audios/jurema/mestre/XXXXXXXXX", letra:"Letra do ponto..."}, ] }, { nome: "Mestre Zé Bebinho", pontos: [ {titulo:"Ponto 01", audio:"audios/jurema/mestre/XXXXXXXXX", letra:"Letra do ponto..."}, {titulo:"Ponto 02", audio:"audios/jurema/mestre/XXXXXXXXX", letra:"Letra do ponto..."}, ] }, { nome: "Mestre Zé Cego", pontos: [ {titulo:"Ponto 01", audio:"audios/jurema/mestre/XXXXXXXXX", letra:"Letra do ponto..."}, {titulo:"Ponto 02", audio:"audios/jurema/mestre/XXXXXXXXX", letra:"Letra do ponto..."}, ] }, { nome: "Mestre Zé da Virada", pontos: [ {titulo:"Ponto 01", audio:"audios/jurema/mestre/XXXXXXXXX", letra:"Letra do ponto..."}, {titulo:"Ponto 02", audio:"audios/jurema/mestre/XXXXXXXXX", letra:"Letra do ponto..."}, ] }, { nome: "Mestre Antõnio Pelintra", pontos: [ {titulo:"Ponto 01", audio:"audios/jurema/mestre/XXXXXXXXX", letra:"Letra do ponto..."}, {titulo:"Ponto 02", audio:"audios/jurema/mestre/XXXXXXXXX", letra:"Letra do ponto..."}, ] }, { nome: "Mestre Zé Pelintra", pontos: [ {titulo:"Chapéu Amarelo...", audio:"audios/jurema/mestre/01-mestre-ze-pelintra", letra: "Chapéu amarelo que vem do Pará (2x) Seu Zé vem Triunfando na mesa real (2x) / Zé Pelintra, Zé Pelintra de Santana Ê (2x) / Deus lhe deu maior poder da maior força que tem (2x)"}, {titulo:"Ponto 02", audio:"audios/jurema/mestre/XXXXXXXXX", letra:"Letra do ponto..."}, ] }, { nome: "Mestre Zé Pereira", pontos: [ {titulo:"Ponto 01", audio:"audios/jurema/mestre/01-Zé-Pereira", letra:"Pereiro Brabo (2x) Meu Pereiro Balançou..."}, {titulo:"Ponto 02", audio:"audios/jurema/mestre/XXXXXXXXX", letra:"Letra do ponto..."}, ] }, { nome: "Mestre Pilão Deitado", pontos: [ {titulo:"Ponto 01", audio:"audios/jurema/mestre/01-pilao-deitado", letra:"Letra do ponto..."}, {titulo:"Ponto 02", audio:"audios/jurema/mestre/XXXXXXXXX", letra:"Letra do ponto..."}, ] }, { nome: "Mestre Nego Gerso", pontos: [ {titulo:"Ponto 01", audio:"audios/jurema/mestre/XXXXXXXXX", letra:"Letra do ponto..."}, {titulo:"Ponto 02", audio:"audios/jurema/mestre/XXXXXXXXX", letra:"Letra do ponto..."}, ] }, { nome: "Mestre Manoel Quebra Pedra", pontos: [ {titulo:"Ponto 01", audio:"audios/jurema/mestre/XXXXXXXXX", letra:"Letra do ponto..."}, {titulo:"Ponto 02", audio:"audios/jurema/mestre/XXXXXXXXX", letra:"Letra do ponto..."}, ] }, { nome: "Mestre Manoel da Serra", pontos: [ {titulo:"Ponto 01", audio:"audios/jurema/mestre/XXXXXXXXX", letra:"Letra do ponto..."}, {titulo:"Ponto 02", audio:"audios/jurema/mestre/XXXXXXXXX", letra:"Letra do ponto..."}, ] }, { nome: "Mestre Manoel Paturi", pontos: [ {titulo:"Ponto 01", audio:"audios/jurema/mestre/XXXXXXXXX", letra:"Letra do ponto..."}, {titulo:"Ponto 02", audio:"audios/jurema/mestre/XXXXXXXXX", letra:"Letra do ponto..."}, ] }, { nome: "Mestre Manoel de Almeida", pontos: [ {titulo:"Ponto 01", audio:"audios/jurema/mestre/XXXXXXXXX", letra:"Letra do ponto..."}, {titulo:"Ponto 02", audio:"audios/jurema/mestre/XXXXXXXXX", letra:"Letra do ponto..."}, ] } ] }, { nome: "Mestras", entidades: [ { nome: "Mestra Emericiana", pontos: [ {titulo:"Ponto 01", audio:"audios/jurema/mestra/XXXXXXXXX", letra:"Letra do ponto..."}, {titulo:"Ponto 02", audio:"audios/jurema/mestra/XXXXXXXXX", letra:"Letra do ponto..."}, ] }, { nome: "Mestra Luziaria", pontos: [ {titulo:"Ponto 01", audio:"audios/jurema/mestra/XXXXXXXXX", letra:"Letra do ponto..."}, {titulo:"Ponto 02", audio:"audios/jurema/mestra/XXXXXXXXX", letra:"Letra do ponto..."}, ] } ] }, { nome: "Caboclos", entidades: [ { nome: "Caboclo Sete Flecha", pontos: [ {titulo:"Ponto 01", audio:"audios/jurema/caboclos/XXXXXXXX", letra:"Letra do ponto..."}, {titulo:"Ponto 02", audio:"audios/jurema/caboclos/XXXXXXXX", letra:"Letra do ponto..."}, ] }, { nome: "Caboclo Cobra Coral", pontos: [ {titulo:"Ponto 01", audio:"audios/jurema/caboclos/XXXXXXXX", letra:"Letra do ponto..."}, {titulo:"Ponto 02", audio:"audios/jurema/caboclos/XXXXXXXX", letra:"Letra do ponto..."}, ] }, { nome: "Caboclo Pena Branca", pontos: [ {titulo:"Ponto 01", audio:"audios/jurema/caboclos/XXXXXXXX", letra:"Letra do ponto..."}, {titulo:"Ponto 02", audio:"audios/jurema/caboclos/XXXXXXXX", letra:"Letra do ponto..."}, ] }, { nome: "Caboclo Arranca Toco", pontos: [ {titulo:"Ponto 01", audio:"audios/jurema/caboclos/XXXXXXXX", letra:"Letra do ponto..."}, {titulo:"Ponto 02", audio:"audios/jurema/caboclos/XXXXXXXX", letra:"Letra do ponto..."}, ] }, { nome: "Caboclo Roxo", pontos: [ {titulo:"Ponto 01", audio:"audios/jurema/caboclos/XXXXXXXX", letra:"Letra do ponto..."}, {titulo:"Ponto 02", audio:"audios/jurema/caboclos/XXXXXXXX", letra:"Letra do ponto..."}, ] } ] }, {nome: "Ciganos", entidades:[]}, {nome: "Pretos Velhos", entidades:[]}, {nome: "Pontos de Mesa", entidades:[]}, {nome: "Malunguinho", entidades:[]} ];
+const acervoJurema = [
+  {
+    nome: "Mestres",
+    entidades: [
+      { nome: "Mestre Antõnio Pelintra", pontos: [
+          {titulo:"Ponto 01", audio:"audios/jurema/mestre/XXXXXXXXX", letra:"Letra do ponto..."},
+          {titulo:"Ponto 02", audio:"audios/jurema/mestre/XXXXXXXXX", letra:"Letra do ponto..."},
+      ] },
+      { nome: "Mestre Cagaruçu", pontos: [
+          {titulo:"Ponto 01", audio:"audios/jurema/mestre/XXXXXXXXX", letra:"Letra do ponto..."},
+          {titulo:"Ponto 02", audio:"audios/jurema/mestre/XXXXXXXXX", letra:"Letra do ponto..."},
+      ] },
+      { nome: "Mestre Manoel de Almeida", pontos: [
+          {titulo:"Ponto 01", audio:"audios/jurema/mestre/XXXXXXXXX", letra:"Letra do ponto..."},
+          {titulo:"Ponto 02", audio:"audios/jurema/mestre/XXXXXXXXX", letra:"Letra do ponto..."},
+      ] },
+      { nome: "Mestre Manoel da Serra", pontos: [
+          {titulo:"Ponto 01", audio:"audios/jurema/mestre/XXXXXXXXX", letra:"Letra do ponto..."},
+          {titulo:"Ponto 02", audio:"audios/jurema/mestre/XXXXXXXXX", letra:"Letra do ponto..."},
+      ] },
+      { nome: "Mestre Manoel Paturi", pontos: [
+          {titulo:"Ponto 01", audio:"audios/jurema/mestre/XXXXXXXXX", letra:"Letra do ponto..."},
+          {titulo:"Ponto 02", audio:"audios/jurema/mestre/XXXXXXXXX", letra:"Letra do ponto..."},
+      ] },
+      { nome: "Mestre Manoel Quebra Pedra", pontos: [
+          {titulo:"Ponto 01", audio:"audios/jurema/mestre/XXXXXXXXX", letra:"Letra do ponto..."},
+          {titulo:"Ponto 02", audio:"audios/jurema/mestre/XXXXXXXXX", letra:"Letra do ponto..."},
+      ] },
+      { nome: "Mestre Nego Gerso", pontos: [
+          {titulo:"Ponto 01", audio:"audios/jurema/mestre/XXXXXXXXX", letra:"Letra do ponto..."},
+          {titulo:"Ponto 02", audio:"audios/jurema/mestre/XXXXXXXXX", letra:"Letra do ponto..."},
+      ] },
+      { nome: "Mestre Pilão Deitado", pontos: [
+          {titulo:"Ponto 01", audio:"audios/jurema/mestre/01-pilao-deitado", letra:"Letra do ponto..."},
+          {titulo:"Ponto 02", audio:"audios/jurema/mestre/XXXXXXXXX", letra:"Letra do ponto..."},
+      ] },
+      { nome: "Mestre Zé Bebinho", pontos: [
+          {titulo:"Ponto 01", audio:"audios/jurema/mestre/XXXXXXXXX", letra:"Letra do ponto..."},
+          {titulo:"Ponto 02", audio:"audios/jurema/mestre/XXXXXXXXX", letra:"Letra do ponto..."},
+      ] },
+      { nome: "Mestre Zé Cego", pontos: [
+          {titulo:"Ponto 01", audio:"audios/jurema/mestre/XXXXXXXXX", letra:"Letra do ponto..."},
+          {titulo:"Ponto 02", audio:"audios/jurema/mestre/XXXXXXXXX", letra:"Letra do ponto..."},
+      ] },
+      { nome: "Mestre Zé da Virada", pontos: [
+          {titulo:"Ponto 01", audio:"audios/jurema/mestre/XXXXXXXXX", letra:"Letra do ponto..."},
+          {titulo:"Ponto 02", audio:"audios/jurema/mestre/XXXXXXXXX", letra:"Letra do ponto..."},
+      ] },
+      { nome: "Mestre Zé Pelintra", pontos: [
+          {titulo:"Chapéu Amarelo...", audio:"audios/jurema/mestre/01-mestre-ze-pelintra", letra: "Chapéu amarelo que vem do Pará (2x) Seu Zé vem Triunfando na mesa real (2x) / Zé Pelintra, Zé Pelintra de Santana Ê (2x) / Deus lhe deu maior poder da maior força que tem (2x)"},
+          {titulo:"Ponto 02", audio:"audios/jurema/mestre/XXXXXXXXX", letra:"Letra do ponto..."},
+      ] },
+      { nome: "Mestre Zé Pereira", pontos: [
+          {titulo:"Ponto 01", audio:"audios/jurema/mestre/01-Zé-Pereira", letra:"Pereiro Brabo (2x) Meu Pereiro Balançou..."},
+          {titulo:"Ponto 02", audio:"audios/jurema/mestre/XXXXXXXXX", letra:"Letra do ponto..."},
+      ] },
+      { nome: "Mestre Zé de Santana", pontos: [
+          {titulo:"Ponto 01", audio:"audios/jurema/mestre/XXXXXXXXX", letra:"Letra do ponto..."},
+          {titulo:"Ponto 02", audio:"audios/jurema/mestre/XXXXXXXXX", letra:"Letra do ponto..."},
+      ] }
+    ]
+  },
+  {
+    nome: "Mestras",
+    entidades: [
+      { nome: "Mestra Emericiana", pontos: [
+          {titulo:"Ponto 01", audio:"audios/jurema/mestra/XXXXXXXXX", letra:"Letra do ponto..."},
+          {titulo:"Ponto 02", audio:"audios/jurema/mestra/XXXXXXXXX", letra:"Letra do ponto..."},
+      ] },
+      { nome: "Mestra Luziaria", pontos: [
+          {titulo:"Ponto 01", audio:"audios/jurema/mestra/XXXXXXXXX", letra:"Letra do ponto..."},
+          {titulo:"Ponto 02", audio:"audios/jurema/mestra/XXXXXXXXX", letra:"Letra do ponto..."},
+      ] }
+    ]
+  },
+  {
+    nome: "Caboclos",
+    entidades: [
+      { nome: "Caboclo Arranca Toco", pontos: [
+          {titulo:"Ponto 01", audio:"audios/jurema/caboclos/XXXXXXXX", letra:"Letra do ponto..."},
+          {titulo:"Ponto 02", audio:"audios/jurema/caboclos/XXXXXXXX", letra:"Letra do ponto..."},
+      ] },
+      { nome: "Caboclo Cobra Coral", pontos: [
+          {titulo:"Ponto 01", audio:"audios/jurema/caboclos/XXXXXXXX", letra:"Letra do ponto..."},
+          {titulo:"Ponto 02", audio:"audios/jurema/caboclos/XXXXXXXX", letra:"Letra do ponto..."},
+      ] },
+      { nome: "Caboclo Pena Branca", pontos: [
+          {titulo:"Ponto 01", audio:"audios/jurema/caboclos/XXXXXXXX", letra:"Letra do ponto..."},
+          {titulo:"Ponto 02", audio:"audios/jurema/caboclos/XXXXXXXX", letra:"Letra do ponto..."},
+      ] },
+      { nome: "Caboclo Roxo", pontos: [
+          {titulo:"Ponto 01", audio:"audios/jurema/caboclos/XXXXXXXX", letra:"Letra do ponto..."},
+          {titulo:"Ponto 02", audio:"audios/jurema/caboclos/XXXXXXXX", letra:"Letra do ponto..."},
+      ] },
+      { nome: "Caboclo Sete Flecha", pontos: [
+          {titulo:"Ponto 01", audio:"audios/jurema/caboclos/XXXXXXXX", letra:"Letra do ponto..."},
+          {titulo:"Ponto 02", audio:"audios/jurema/caboclos/XXXXXXXX", letra:"Letra do ponto..."},
+      ] }
+    ]
+  },
+  { nome: "Ciganos", entidades: [] },
+  { nome: "Pretos Velhos", entidades: [] },
+  { nome: "Pontos de Mesa", entidades: [] },
+  { nome: "Malunguinho", entidades: [] }
+];
