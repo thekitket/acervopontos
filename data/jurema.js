0e111bf6794b95a495b05ed9b824f3191b991228
Kethuly Aline <66758415+thekitket@users.noreply.github.com>
2026-01-22 02:17:06 -0300

    Create jurema.js

---
+++ data/jurema.js
@@ -0,0 +1,29 @@
+const acervoJurema = [
+  {
+    nome: "Mestres",
+    entidades: [
+      {
+        nome: "Mestre Zé Pelintra",
+        pontos: [
+          {titulo:"Chapéu Amarelo", audio:"assets/audio/jurema/mestres/chapeu-amarelo", letra:"Chapéu amarelo que vem do Pará..."},
+          {titulo:"Outro Ponto", audio:"assets/audio/jurema/mestres/outro", letra:"Letra do outro ponto..."}
+        ]
+      }
+    ]
+  },
+  {
+    nome: "Caboclos",
+    entidades: [
+      {
+        nome: "Caboclo da Jurema",
+        pontos: [
+          {titulo:"Caboclo da Mata", audio:"assets/audio/jurema/caboclos/caboclo-da-mata", letra:"Ele vem da mata..."}
+        ]
+      }
+    ]
+  },
+  {nome: "Ciganos", entidades:[]},
+  {nome: "Pretos Velhos", entidades:[]},
+  {nome: "Pontos de Mesa", entidades:[]},
+  {nome: "Malunguinho", entidades:[]}
+];
