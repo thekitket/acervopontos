c93e3ceff66c34ccf1bfe5df20813e35fa0f7c1a
Kethuly Aline <66758415+thekitket@users.noreply.github.com>
2026-01-22 02:25:08 -0300

    Create exu.js

---
+++ data/exu.js
@@ -0,0 +1,24 @@
+const acervoExu = [
+  {
+    nome: "Exu",
+    entidades: [
+      {
+        nome: "Exu Tranca-Rua",
+        pontos: [
+          {titulo:"Ponto de Tranca-Rua", audio:"assets/audio/exu/exu-tranca-rua/ponto1", letra:"Letra do ponto de Exu Tranca-Rua..."}
+        ]
+      }
+    ]
+  },
+  {
+    nome: "Pombo Gira",
+    entidades: [
+      {
+        nome: "Pomba Gira Maria Padilha",
+        pontos: [
+          {titulo:"Ponto Maria Padilha", audio:"assets/audio/exu/pombogira/maria-padilha", letra:"Letra do ponto de Maria Padilha..."}
+        ]
+      }
+    ]
+  }
+];
