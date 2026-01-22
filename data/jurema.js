08ceed52fc501c56ec2dbd9903917c6b6c9b6a9b
Kethuly Aline <66758415+thekitket@users.noreply.github.com>
2026-01-22 03:06:27 -0300

    Update jurema.js

--- data/jurema.js
+++ data/jurema.js
@@ -5,7 +5,7 @@ const acervoJurema = [
       {
         nome: "Mestre Zé Pelintra",
         pontos: [
-          {titulo:"Chapéu Amarelo...", audio:"audios/jurema/mestre/01-mestre-ze-pelintra", letra:"Chapéu amarelo que vem do Pará..."},
+          {titulo:"Chapéu Amarelo...", audio:"audios/jurema/mestre/01-mestre-ze-pelintra", letra: "Chapéu amarelo que vem do Pará (2x) / Seu Zé vem Triunfando na mesa real"},
           {titulo:"Outro Ponto", audio:"assets/audio/jurema/mestres/outro", letra:"Letra do outro ponto..."},
           {titulo:"Outro Ponto 2", audio:"assets/audio/jurema/mestres/outro2", letra:"Letra do outro ponto 2..."}
         ]
